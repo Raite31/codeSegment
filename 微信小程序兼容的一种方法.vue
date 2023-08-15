f1aa45706f31ea825aa99469c15a9e1e209d2358
李嘉胜 <60214514+Raite31@users.noreply.github.com>
2023-08-15 09:23:12 +0800

    微信小程序兼容的一种方法

---
+++ 微信小程序兼容的一种方法.vue
@@ -0,0 +1,30 @@
+<view
+    class="header-box"
+    :class="iphonexup ? 'phonexup' : ' '"
+>
+</view>
+
+//====================== script
+data(){
+    return{
+        // 处理兼容
+        iphonexup: false,
+        iphone14: false
+    }
+},
+
+onReady() {
+    const deviceModel = wx.getSystemInfoSync().model;
+    console.log("型号： ",deviceModel)
+        
+    if (deviceModel.includes('iPhone X') || deviceModel.includes('iPhone 11') || deviceModel.includes('iPhone 12') || deviceModel.includes('iPhone 13') || deviceModel.includes('iPhone 14')) {
+        console.log(1111111111)
+        this.iphonexup = true
+        
+    }
+},
+
+//====================== style
+.phonexup{
+	padding-top: 50rpx;
+}
