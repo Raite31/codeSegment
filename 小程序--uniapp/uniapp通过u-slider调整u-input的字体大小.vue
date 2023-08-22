d6f21ee38887c4fb80d76ff6742eaead67768a11
李嘉胜 <2330165939@qq.com>
2023-08-22 09:15:11 +0800

    Create uniapp通过u-slider调整u-input的字体大小.vue

---
+++ 小程序--uniapp/uniapp通过u-slider调整u-input的字体大小.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<view
+			class="flex_bd"
+			style="border-bottom: 2rpx solid rgba(207, 207, 207, 1)"
+		>
+			<!-- 可以看到这里的input有:fontSize这一项，这里需要改动u-input的源码 -->
+			<u-input
+				:fontSize="font_size"
+				v-model="data.list[switchIndex][tabCurrentIndex].text"
+				placeholder="输入文字内容"
+				type="text"
+			/>
+		</view>
+
+		<view class="slider">
+			<slider min="28" show-value @changing="sliderChange"></slider>
+		</view>
+	</view>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			// 控制文字大小
+			font_size: 28,
+		};
+	},
+	methods: {
+		sliderChange(e) {
+			console.log('value 发生变化：' + e.detail.value);
+			this.font_size = e.detail.value;
+		},
+	},
+};
+</script>
