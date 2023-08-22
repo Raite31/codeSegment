44a4d9ed3c7818263c9da9b8c2a68f5912b1a157
李嘉胜 <60214514+Raite31@users.noreply.github.com>
2023-08-22 04:18:01 -0500

    Create uniapp onBackPress失效时的代替品.vue

---
+++ 小程序--uniapp/uniapp onBackPress失效时的代替品.vue
@@ -0,0 +1,10 @@
+// uniapp的onBackPress特性：当前页面栈 大于 1 才生效。如第一次就打开 用户中心 页面，然后从 用户中心 redirectTo 跳转到登录页，这时在登录页 的 onBackPress 是不会触发的
+
+onUnload() {  
+    // #ifdef MP-WEIXIN  
+        //监听返回的逻辑  
+    if (this.isEditTemplate) {  
+        this.$refs.alertDialog.open();  
+    }  
+    // #endif  
+},
