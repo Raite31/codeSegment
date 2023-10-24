f44de6472886e67fd2bdd884ea256e135034b8d4
李嘉胜 <2330165939@qq.com>
2023-10-24 16:47:01 +0800

    update

---
+++ 小程序--uniapp/单双数孩子的样式.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="list">
+    <div class="item"></div>
+  </div>
+</template>
+<style lang="scss">
+.list {
+  & > :nth-child(2n + 1) {
+    margin-right: 46rpx;
+  }
+}
+</style>
---
+++ 小程序--uniapp/安卓软键盘把按钮顶起问题.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="contain">
+		<view> </view>
+		<view> </view>
+		<view> </view>
+		<button>提交</button>
+	</div>
+</template>
+<style lang="scss">
+// 首先 不能用position: fixed，这个属性就会导致这样的问题
+.contain {
+	button {
+		position: absolute;
+		bottom: 20%;
+		&::after {
+			border: none;
+		}
+	}
+}
+</style>
