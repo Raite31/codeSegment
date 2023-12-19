c2f126eac9810981f3393168438010bd857d567e
李嘉胜 <2330165939@qq.com>
2023-12-19 11:42:07 +0800

    update

--- uniapp/小程序页面头.vue
+++ uniapp/小程序页面头.vue
@@ -1,49 +1,58 @@
 <template>
-    <!-- 顶部栏 -->
-    <view
-        class="header-box"
-        style="padding: 0 20rpx; position: relative"
-    >
-        <view style="position: absolute; left: 20rpx">
-            <u-icon
-                class="ml24 mr254 mt40 header-icon"
-                name="arrow-left"
-                size="38"
-                @click="app.goPage(-1)"
-            >
-            </u-icon>
-        </view>
-        <!-- #ifndef H5 -->
-        <view class="window-top mb40"></view>
-        <!-- #endif -->
-        <view
-            class="fs34 text-center"
-            style="width: 100%; text-align: center"
-        >
-            {{ app.langReplace('确认订单') }}
-        </view>
-    </view>
+	<!-- 顶部栏 -->
+	<view class="header-box">
+		<view class="header-box-left">
+			<u-icon class="header-icon" name="arrow-left" size="38" @click="app.goPage(-1)">
+			</u-icon>
+		</view>
 
-    <view class="contain">
-    </view>
+		<!-- #ifndef H5 -->
+		<view class="window-top mb40"></view>
+		<!-- #endif -->
+
+		<view class="header-box-center">
+			你好你好
+		</view>
+
+		<view class="header-box-right">
+			记录
+		</view>
+	</view>
+
+	<view class="contain">
+	</view>
 </template>
 
 
 <style lang="scss">
 .header-box {
 	display: flex;
+	align-items: center;
+
 	height: 100rpx;
 	background: #fff;
 	color: #000;
-	.header-icon{
-		margin-top: 35rpx !important;
+	padding: 0 20rpx;
+
+	.header-box-left {
+		left: 20rpx;
 	}
-	.text-center{
-		padding: 30rpx;
+
+	.header-box-center {
+		flex: 1;
+		text-align: center;
+		font-size: 34rpx;
 	}
-}
-.contain{
-    position: relative;
-    top: -1200rpx;
+
+	.header-box-right {
+		font-size: 24rpx;
+		font-family: PingFang SC, PingFang SC;
+		font-weight: 400;
+		color: rgba(0, 0, 0, 0.9);
+		line-height: 28rpx;
+
+		align-self: flex-start;
+		padding-top: 35rpx;
+	}
 }
 </style>
