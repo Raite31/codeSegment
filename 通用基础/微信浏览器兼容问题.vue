4dbdd1439e4a409934e30818ba03f856983761b6
李嘉胜 <2330165939@qq.com>
2023-12-21 13:35:25 +0800

    update

---
+++ 通用基础/微信浏览器兼容问题.vue
@@ -0,0 +1,4 @@
+<!-- 在微信浏览器中，加一个元素给高度是无法撑起来的，要用margin -->
+<view style="height: 0rpx"></view>
+<view class="diy-fixedsearch fixed" :style="app.getPlatform() == 'H5-WX' ? {marginTop: '148rpx'} : {marginTop: '68rpx'}" >
+</view>
